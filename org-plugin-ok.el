a36c0de753c71848faabcef2cfb9d4ef004da6a5
Taro Sato <okomestudio@gmail.com>
2024-10-13 23:51:52 -0700

    Update org-plugin-ok.el

--- org-plugin-ok.el
+++ org-plugin-ok.el
@@ -1,1 +1,34 @@
+;;; org-plugin-ok.el --- Org Plugin  -*- lexical-binding: t -*-
+;;
+;; Copyright (C) 2024 Taro Sato
+;;
+;; Author: Taro Sato <okomestudio@gmail.com>
+;; URL: https://github.com/okomestudio/org-roam-plugin-ok
+;; Version: 0.1
+;; Keywords: org-mode, plug-in
+;; Package-Requires: ((emacs "29.1") (org "9.7"))
+;;
+;;; License:
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;;
+;;; Commentary:
+;;
+;; The `org-plugin-ok' mode is a plugin to enhance `org' in
+;; several ways. See the repository README for detail.
+;;
+;;; Code:
 
+(provide 'org-plugin-ok)
+;;; org-plugin-ok.el ends here
