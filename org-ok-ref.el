6f8ce13fc9812b035fbc461e3b9aceb03e2df8ca
Taro Sato <okomestudio@gmail.com>
2025-08-03 18:49:49 -0700

    Refactor slug usage

--- org-ok-ref.el
+++ org-ok-ref.el
@@ -4,18 +4,18 @@
 ;;
 ;;; License:
 ;;
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 of the License, or (at your option) any later
+;; version.
 ;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
 ;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;; You should have received a copy of the GNU General Public License along with
+;; this program. If not, see <https://www.gnu.org/licenses/>.
 ;;
 ;;; Commentary:
 ;;
@@ -23,6 +23,7 @@
 ;;
 ;;; Code:
 
+(require 'ok)
 (require 'org-ref)
 
 (defun org-ok-ref--define-accessors (record)
@@ -36,7 +37,7 @@
 
   (defun org-ok-ref-record-slug (&optional key)
     "Get the slug for the current Bibtex record."
-    (org-ok-text-to-slug (org-ok-ref-record-title key)))
+    (ok-string-text-to-slug (org-ok-ref-record-title key)))
 
   (defun org-ok-ref-record-title (&optional key)
     "Get the title for the current Bibtex record."
--- org-ok.el
+++ org-ok.el
@@ -4,29 +4,29 @@
 ;;
 ;; Author: Taro Sato <okomestudio@gmail.com>
 ;; URL: https://github.com/okomestudio/org-ok
-;; Version: 0.2.1
+;; Version: 0.3.1
 ;; Keywords: org-mode, plug-in, convenience
-;; Package-Requires: ((emacs "30.1") (dash "2.13") (org "9.7") (org-ref "3.1"))
+;; Package-Requires: ((emacs "30.1") (dash "2.13") (ok "0.12.1") (org "9.7") (org-ref "3.1"))
 ;;
 ;;; License:
 ;;
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
+;; This program is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, either version 3 of the License, or (at your option) any later
+;; version.
 ;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
+;; This program is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+;; FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+;; details.
 ;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+;; You should have received a copy of the GNU General Public License along with
+;; this program. If not, see <https://www.gnu.org/licenses/>.
 ;;
 ;;; Commentary:
 ;;
-;; The `org-ok' mode is a plugin to enhance `org' in several ways. See
-;; the repository README for detail.
+;; The `org-ok' mode is a plugin to enhance `org' in several ways. See the
+;; repository README for detail.
 ;;
 ;;; Code:
 
