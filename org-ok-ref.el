481824428e3d3da3008541815d4ce3585069116a
Taro Sato <okomestudio@gmail.com>
2025-09-17 16:42:54 -0700

    Provide a richer set of reference styles

--- org-ok-ref.el
+++ org-ok-ref.el
@@ -28,6 +28,24 @@
 (require 'org-ref)
 (require 's)
 
+;;; MULE utility
+
+(defvar ok-mule-s nil)
+
+(defun ok-mule-lang-p (lang)
+  "TBD."
+  (if (eq lang 'ja)
+      current-input-method))
+
+(defun ok-mule-s (text &optional lang)
+  "Render TEXT in the current language.
+If LANG, a two-char symbol, is not given, it will be auto-discovered, currently
+`ja' only. The default is `en'."
+  (let ((lang (or lang (if current-input-method 'ja 'en))))
+    (alist-get lang (alist-get text ok-mule-s nil nil #'equal) text)))
+
+;;;
+
 (defun org-ok-ref-format-author (authors)
   "Format BibTeX AUTHORS list.
 See `bibtex-completion-shorten-authors' for reference."
@@ -42,55 +60,123 @@ See `bibtex-completion-shorten-authors' for reference."
                       (concat (car p) (cadr p))
                     (concat (cadr p) " " (car p)))))
 
-(defun org-ok-ref-title--sanitize (title)
+(push '("%s et al." . ((ja . "%s他"))) ok-mule-s)
+(push '("%s & %s" . ((ja . "%s＆%s"))) ok-mule-s)
+(push '("%s %s" . ((ja . "%s%s"))) ok-mule-s)
+(push '("${authors-full} /[[${citekey}][${title}]]/ (${publisher}, ${year})"
+        . ((ja . "${authors-full}『[[${citekey}][${title}]]』（${publisher}、${year}）")))
+      ok-mule-s)
+
+(defun org-ok-ref-name--etal (names)
+  "Format NAMES with et al."
+  (cond ((= (length names) 1)
+         (nth 0 names))
+        ((= (length names) 2)
+         (format (ok-mule-s "%s & %s")
+                 (nth 0 names) (nth 1 names)))
+        ((>= (length names) 3)
+         (format (ok-mule-s "%s et al.")
+                 (nth 0 names)))
+        (t "N/A")))
+
+(defun org-ok-ref-bibtex--sanitize-title (title)
   "Sanitize string TITLE for display."
   (setq title (replace-regexp-in-string "{{\\([^}]+\\)}}" "\\1" title))
-  (setq title (replace-regexp-in-string "\\\\&" "&" title))
-  title)
+  (replace-regexp-in-string "\\\\&" "&" title))
 
-(defun org-ok-ref-link-description (key style)
-  "Render link description for cite KEY in STYLE."
-  (let* ((bibentry (bibtex-completion-get-entry key))
-         (authors (or (bibtex-completion-get-value "author" bibentry)
-                      (bibtex-completion-get-value "editor" bibentry)))
-         (year (bibtex-completion-get-value "date" bibentry "N/A")))
-    (setq authors (--map (if (string-match "\\(.*\\),.*" it)
-                             (format "%s" (match-string 1 it))
-                           it)
-                         (split-string authors " and ")))
-    (setq authors (cond ((= (length authors) 1)
-                         (nth 0 authors))
-                        ((= (length authors) 2)
-                         (format "%s & %s" (nth 0 authors) (nth 1 authors)))
-                        ((>= (length authors) 3)
-                         (format "%s et al." (nth 0 authors)))
-                        (t "N/A")))
+(defun org-ok-ref-bibtex--sanitize-person-name (name)
+  "Sanitize string person's NAME for display."
+  (replace-regexp-in-string "{\\([^}]+\\)}" "\\1" name))
+
+(defun org-ok-ref-bibtex-entry (key)
+  "Retrieve BibTeX entry with KEY as an alist."
+  (let* ((entry (bibtex-completion-get-entry key))
+         (person-names
+          (--map (org-ok-ref-bibtex--sanitize-person-name it)
+                 (split-string
+                  (or (bibtex-completion-get-value "author" entry)
+                      (bibtex-completion-get-value "editor" entry))
+                  " and ")))
+         (title (org-ok-ref-bibtex--sanitize-title
+                 (bibtex-completion-get-value "title" entry)))
+         (publisher (bibtex-completion-get-value "publisher" entry ""))
+         (year (bibtex-completion-get-value "date" entry "N/A"))
+         authors-full)
     (setq year (if (string-match "\\([0-9]\\{4\\}\\)" year)
                    (match-string 1 year)
                  year))
-    (pcase style
-      ('title (org-ok-ref-title--sanitize
-               (bibtex-completion-get-value "title" bibentry)))
-      ('short-paren (format "%s (%s)" authors year))
-      ('short (format "%s %s" authors year))
-      ('long (format "%s" authors))
-      ('full (bibtex-completion-apa-format-reference key)))))
+    (setq authors (--map (if (string-match "\\(.*\\),.*" it)
+                             (format "%s" (match-string 1 it))
+                           it)
+                         person-names))
+    (setq authors-full
+          (--map (if (string-match "\\(.*\\), \\(.*\\)\\s-*" it)
+                     (format (ok-mule-s "%s %s")
+                             (match-string (if (ok-mule-lang-p 'ja) 1 2) it)
+                             (match-string (if (ok-mule-lang-p 'ja) 2 1) it))
+                   it)
+                 person-names))
+    (setq authors (org-ok-ref-name--etal authors))
+    (setq authors-full (org-ok-ref-name--etal authors-full))
+
+    (list (cons "authors" authors)
+          (cons "authors-full" authors-full)
+          (cons "title" title)
+          (cons "publisher" publisher)
+          (cons "year" year))))
+
+(defun org-ok-ref-format-string--apa (key)
+  (format "[[${citekey}][%s]]" (bibtex-completion-apa-format-reference key)))
+
+(defun org-ok-ref-format-string--full-inline (key)
+  (ok-mule-s "${authors-full} /[[${citekey}][${title}]]/ (${publisher}, ${year})"))
+
+(defvar org-ok-ref-format-string
+  '((book
+     . ((apa . org-ok-ref-format-string--apa)
+        (author-year-paren . "[[${citekey}][${authors} (${year})]]")
+        (author-year . "[[${citekey}][${authors} ${year}]]")
+        (full-inline . org-ok-ref-format-string--full-inline)))
+    (_
+     . ((apa . org-ok-ref-format-string--apa)
+        (author-year-paren . "[[${citekey}][${authors} (${year})]]")
+        (author-year . "[[${citekey}][${authors} ${year}]]")
+        (full-inline . org-ok-ref-format-string--full-inline))))
+  "String formats for BibTeX entry types.")
 
 (defun org-ok-ref-link-insert (&optional _arg)
-  "Insert or update a cite link.
-This command insert a new link or updates an existing link if the point is on a
-link.
+  "Insert or update a cite link with a reference description.
+When the point is on an existing link, the citekey is used to get the reference
+key. Otherwise, the command will prompt for a key.
 
-The prefix argument sets the style for `org-ok-ref-link-description':
+When the point is on an existing link, the description will be updated based on
+the user request. Otherwise, a new link will be created.
 
-- 1: title
-- 4: short-paren
-- 16: short
-- 64: long
+Use the prefix argument to choose the style for description:
 
-See the help for `org-ok-ref-link-description' for the descriptions of available styles."
+  - 1: 'Authors (Year)'
+  - 2: 'Authors Year'
+  - 3: APA
+  - 4: Trigger prompt for interactive selection
+
+The default style is 'Authors _Title_ (Publisher, Year)'."
   (interactive "P")
-  (let* (range
+  (let* ((style
+          (pcase _arg
+            ('1 'author-year-paren)
+            ('2 'author-year)
+            ('3 'apa)
+            ((or '4 '(4))
+             (let ((collection
+                    '(("APA" . apa)
+                      ("Authors _Title_ (Publisher, Year)" . full-inline)
+                      ("Authors (Year)" . author-year-paren)
+                      ("Authors Year" . author-year))))
+               (alist-get (completing-read "Reference style: "
+                                           collection nil t)
+                          collection nil nil #'equal)))
+            (_ 'full-inline)))
+         (range nil)
          (key
           (if-let*
               ((link (and (org-in-regexp org-link-any-re)
@@ -103,19 +189,22 @@ See the help for `org-ok-ref-link-description' for the descriptions of available
                 (setq range (list start end))
                 key)
             (org-ref-read-key)))
-         (style (pcase _arg
-                  ('1 'title)
-                  ('4 'short-paren) ('(4) 'short-paren)
-                  ('16 'short) ('(16) 'short)
-                  ('64 'long) ('(64) 'long)
-                  ('256 'full) ('(256) 'full))))
-    (when-let* ((start (car range))
-                (end (cadr range)))
+         (entry (org-ok-ref-bibtex-entry key))
+         (fstring
+          (alist-get style
+                     (alist-get (bibtex-completion-get-value "=type=" entry)
+                                org-ok-ref-format-string
+                                (alist-get '_ org-ok-ref-format-string)))))
+    (push `("citekey" . ,(format "cite:&%s" key)) entry)
+
+    ;; Delete the existing link
+    (when-let* ((start (car range)) (end (cadr range)))
       (delete-region start end)
       (goto-char start))
-    (org-insert-link nil
-                     (format "cite:&%s" key)
-                     (org-ok-ref-link-description key style))))
+
+    (insert (s-format (if (stringp fstring) fstring (funcall fstring key))
+                      (lambda (field entry) (cdr (assoc-string field entry)))
+                      entry))))
 
 ;;; Obsolete Utility Functions
 ;;
